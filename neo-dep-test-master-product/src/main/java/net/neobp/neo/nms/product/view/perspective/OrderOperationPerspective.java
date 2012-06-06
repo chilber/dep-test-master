f491165e35ab9180887d0bf92f10d70880e7a95a
cahi <cahi@cahi2-nb.neo-partners.com>
2012-06-06 09:33:34 +0200

    removed unused classes

--- neo-dep-test-master-product/src/main/java/net/neobp/neo/nms/product/view/perspective/OrderOperationPerspective.java
+++
@@ -1,9 +0,0 @@
-package net.neobp.neo.nms.product.view.perspective;
-
-import net.neobp.neo.nms.AbstractMobileSuitePerspective;
-
-public class OrderOperationPerspective
-extends AbstractMobileSuitePerspective
-{
-	public static final String ID = "orderOperation";
-}
